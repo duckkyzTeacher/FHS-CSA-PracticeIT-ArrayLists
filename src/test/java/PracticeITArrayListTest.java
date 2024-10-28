fe92a5235eb90d8ca8c09eba2bdfdcfeabeb2c1b
duckkyz <benrc_johnson@yahoo.com>
2024-10-27 22:48:32 -0700

    Another fix

--- src/test/java/PracticeITArrayListTest.java
+++ src/test/java/PracticeITArrayListTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import src.main.java.PracticeITArrayList;
 
 public class PracticeITArrayListTest {
 
